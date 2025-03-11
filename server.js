2518926c3eb6b6a597a8281fab9b22ead4aed38b
Sakthikv <138273283+Sakthikv@users.noreply.github.com>
2025-03-11 22:46:31 +0530

    server.js

--- server.js
+++ server.js
@@ -214,21 +214,7 @@ app.post("/addcart", (req, res) => {
 
     // Validate required fields
     if (!product_id || !student_id || !product_name || !buying_student_id || !product_type || !cost || !url) {
-         if(!product_id)
-            return res.status(400).json({ error: "All fields including image are required pi" });
- if(!student_id)
-        return res.status(400).json({ error: "All fields including image are required si" });
- if(!buying_student_id)
-        return res.status(400).json({ error: "All fields including image are required buying si" });
- if(!product_name)
-        return res.status(400).json({ error: "All fields including image are required product name" });
-    
- if(!product_type)
-        return res.status(400).json({ error: "All fields including image are required type" });
- if(!cost)
-        return res.status(400).json({ error: "All fields including image are required cost" });
- if(!url)
-        return res.status(400).json({ error: "All fields including image are required url" });
+        return res.status(400).json({ error: "All fields including image are required" });
     }
 
     const sql = "INSERT INTO product_cart (product_id, student_id, buying_student_id, product_name, product_type, cost, url) VALUES (?, ?, ?, ?, ?, ?, ?)";
